6d90960d4ce10afba84677c5b5f3a5b07a8d0332
7saw7 <113914373+7saw7@users.noreply.github.com>
2025-05-30 22:22:26 -0500

    Update DecantGallery.js

--- src/components/DecantGallery.js
+++ src/components/DecantGallery.js
@@ -8,7 +8,7 @@ function MiniatureGallery({image, imagetwo, imagethree}) {
         <section className='w-full flex flex-col gap-5'>
             <img src={mainImage} alt="Perfume" className="w-full aspect-square rounded-lg object-cover object-center" />
             <div className='grid grid-cols-3 gap-x-2 md:gap-x-5'>
-                <img src={image} className="w-full object-cover aspect-square rounded-lg cursor-pointer" onClick={()=>setMainImage(image)} />
+                <img src={image} alt="Descripción de la imagen" className="w-full object-cover aspect-square rounded-lg cursor-pointer" onClick={()=>setMainImage(image)} />
                 <img src={imagetwo} alt="Perfume" className="w-full object-cover aspect-square rounded-lg cursor-pointer"  onClick={()=>setMainImage(imagetwo)} />
                 <img src={imagethree} alt="Perfume" className="w-full object-cover aspect-square rounded-lg cursor-pointer"  onClick={()=>setMainImage(imagethree)} />
             </div>
